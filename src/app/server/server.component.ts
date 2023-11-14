747bb9035b4b33ad6c15b591f500344f05502cb5
Rupam <rupampal48@gmail.com>
2023-11-14 11:47:57 +0530

    3rd com - Creating a new component - vid 17

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+//component decorator
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+
+})
+export class ServerComponent {
+
+}
