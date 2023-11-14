7ed5c47dc15014183be5ba686707afd459dcdca5
Rupam <rupampal48@gmail.com>
2023-11-14 14:34:34 +0530

    9th com - working with component selector v23

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: 'app-servers', //element selector
+  // selector: '[app-servers]', //attribute selector
+  // selector: '.app-servers', //class selector
   template: `<app-server></app-server><app-server></app-server>`,
   styleUrl: './servers.component.css'
 })
