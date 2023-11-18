2a7681cc01bdb00ea9a7cfe82e9e57629aa52ed3
Rupam <rupampal48@gmail.com>
2023-11-18 12:28:24 +0530

    23com apply css classes dynamic. with ngClass v41

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,6 +4,10 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
+    styles: `
+    .online{
+        color:white;
+    }`
 
 })
 export class ServerComponent {
